58dd4490e20b72c59ec596597b0044d87deea92d
Frank Gaston <62415557+vinhhung263@users.noreply.github.com>
2022-04-26 13:28:56 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,115 @@
+'use strict'
+
+const express = require('express')
+const Prometheus = require('prom-client')
+
+const app = express()
+const port = process.env.PORT || 3001
+const metricsInterval = Prometheus.collectDefaultMetrics()
+
+//Custom Metrics
+const checkoutsTotal = new Prometheus.Counter({
+    name: 'checkouts_total',
+    help: 'Total number of checkouts',
+    labelNames: ['payment_method']
+})
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
+
+// Runs before each requests
+app.use((req, res, next) => {
+    res.locals.startEpoch = Date.now()
+    next()
+})
+
+app.get('/', (req, res, next) => {
+    setTimeout(() => {
+        res.json({ message: 'Hello World!' })
+        res.statusCode = 200;
+        next()
+
+    }, Math.round(Math.random() * 200))
+})
+
+app.get('/bad', (req, res, next) => {
+    next(new Error('Error Message'))
+})
+
+app.get('/bigdata', (req, res, next) => {
+    const myData = [];
+    for (let i = 0; i < 350000; i++) {
+        myData[i] = "abcdefghijklmnop";
+    }
+
+    res.json({ message: 'Bigdata : ' + myData })
+    res.statusCode = 200;
+    next()
+})
+
+app.get('/lag', (req, res, next) => {
+    setTimeout(() => {
+        res.json({ message: 'Wait a moment!' })
+        res.statusCode = 200;
+        next()
+
+    }, Math.round(Math.random() * 4000))
+})
+
+app.get('/checkout', (req, res, next) => {
+    const paymentMethod = Math.round(Math.random()) === 0 ? 'stripe' : 'paypal'
+
+    checkoutsTotal.inc({
+        payment_method: paymentMethod
+    })
+
+    res.json({ message: 'checked out! By ' + paymentMethod })
+    res.statusCode = 200;
+    next()
+})
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', Prometheus.register.contentType)
+    res.end(Prometheus.register.metrics())
+})
+
+// Error handler
+app.use((err, req, res, next) => {
+    res.statusCode = 500
+    res.json({ error: err.message })
+    next()
+})
+
+// Runs after each requests
+app.use((req, res, next) => {
+    res.on("finish", function () {
+        const responseTimeInMs = Date.now() - res.locals.startEpoch
+
+        httpRequestDurationMicroseconds
+            .labels(req.method, req.route.path, res.statusCode || 200)
+            .observe(responseTimeInMs)
+        next()
+    });
+
+})
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening on http://localhost:${port} !`)
+})
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+    clearInterval(metricsInterval)
+
+    server.close((err) => {
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        }
+
+        process.exit(0)
+    })
+})
